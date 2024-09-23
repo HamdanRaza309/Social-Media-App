e6f7688420a848850128abfb5cc49cde4dd0567c
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-09-23 12:48:31 +0500

    Add follow/unfollow functionality with state updates and success/error handling.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,13 +5,52 @@ import { Link, useParams } from 'react-router-dom';
 import Avatar from "react-avatar";
 import dp from '../assets/profile.png';
 import useGetProfile from '../hooks/useGetProfile';
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios';
+import { USER_API_ENDPOINT } from '../utils/constant';
+import toast from 'react-hot-toast'
+import { getRefresh } from '../redux/tweetSlice';
+import { followingUpdate } from '../redux/userSlice';
 function Profile() {
+    const dispatch = useDispatch();
+
     const { user, profile } = useSelector(store => store.user);
     const { id } = useParams();
     useGetProfile(id);
 
+    const handleFollowAndUnfollow = async () => {
+        if (user.following.includes(id)) {
+            // unfollow
+            try {
+                const res = await axios.post(`${USER_API_ENDPOINT}/unfollow/${id}`, {}, {
+                    withCredentials: true
+                });
+                console.log(res);
+                dispatch(followingUpdate(id));
+                dispatch(getRefresh());
+                toast.success(res.data.message);
+
+            } catch (error) {
+                console.log('Something went wrong while unfollowing.');
+                toast.error(error.response.data.message);
+            }
+        } else {
+            // follow
+            try {
+                const res = await axios.post(`${USER_API_ENDPOINT}/follow/${id}`, {}, {
+                    withCredentials: true
+                });
+                console.log(res);
+                dispatch(followingUpdate(id));
+                dispatch(getRefresh());
+                toast.success(res.data.message);
+
+            } catch (error) {
+                console.log('Something went wrong while following.');
+                toast.error(error.response.data.message);
+            }
+        }
+    }
     return (
         <div className='w-[50%] border-l border-r'>
             <div>
@@ -29,7 +68,15 @@ function Profile() {
                     <Avatar src={dp} size="120" round={true} />
                 </div>
                 <div className=' text-right m-2'>
-                    <button className='px-4 py-1 rounded-full border border-gray-400 hover:bg-gray-200'>Edit Profile</button>
+                    {
+                        user?._id === profile?._id ?
+                            (<button className='px-4 py-1 rounded-full border border-gray-400 hover:bg-gray-200'>Edit Profile</button>)
+                            :
+                            (<button onClick={handleFollowAndUnfollow} className='bg-black text-white px-6 py-1 rounded-full border-2 border-black hover:text-black hover:border-gray-400 hover:bg-gray-200'>
+                                {user.following.includes(id) ? 'Following' : 'Follow'}
+                            </button>)
+                    }
+
                 </div>
                 <div className='m-4'>
                     <h1 className="font-semibold text-xl">{profile?.name || 'Name'}</h1>
